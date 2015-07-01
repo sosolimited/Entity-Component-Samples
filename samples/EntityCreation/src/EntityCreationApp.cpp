8202d92e171472de103e44504b47220254becbfa
David Wicks <david@sosolimited.com>
2015-07-01 13:52:05 -0400

    Add stub entityx classes to sample.

--- samples/EntityCreation/src/EntityCreationApp.cpp
+++ samples/EntityCreation/src/EntityCreationApp.cpp
@@ -2,20 +2,34 @@
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
 
+#include "entityx/System.h"
+
 using namespace ci;
 using namespace ci::app;
 using namespace std;
 
 class EntityCreationApp : public App {
-  public:
+public:
+	EntityCreationApp();
+
 	void setup() override;
 	void mouseDown( MouseEvent event ) override;
 	void update() override;
 	void draw() override;
+private:
+	entityx::EventManager	 events;
+	entityx::EntityManager entities;
+	entityx::SystemManager systems;
 };
+
+EntityCreationApp::EntityCreationApp()
+: entities(events),
+	systems(entities, events)
+{}
 
 void EntityCreationApp::setup()
 {
+
 }
 
 void EntityCreationApp::mouseDown( MouseEvent event )
