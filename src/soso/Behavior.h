5bac80e8138384df4024cb019b0457df186c8429
David Wicks <david@sosolimited.com>
2015-09-23 11:16:12 -0400

    Add behavior lambda.
    Makes it easier to quickly define behaviors in-place.
    Again, like all behaviors, consider whether these should later
    become codified into a component + system pair.

--- src/soso/Behavior.h
+++ src/soso/Behavior.h
@@ -49,11 +49,37 @@ private:
 	entityx::Entity	_entity;
 };
 
+class BehaviorLambda : public BehaviorBase
+{
+public:
+	using Lambda = std::function<void (entityx::Entity, entityx::TimeDelta)>;
+
+	BehaviorLambda(entityx::Entity entity, const Lambda &update_fn)
+	: BehaviorBase(entity),
+		_update_fn(update_fn)
+	{}
+
+	void update(entityx::TimeDelta dt) final override {
+		_update_fn(entity(), dt);
+	}
+
+private:
+	Lambda _update_fn;
+};
+
 /// Construct and assign a behavior of type B to an entity.
 template <typename B, typename ... Params>
 std::shared_ptr<B> assignBehavior( entityx::Entity entity, Params&& ... params )
 {
 	auto behavior = std::make_shared<B>( entity, std::forward<Params>( params ) ... );
+	auto component = entity.has_component<BehaviorComponent>() ? entity.component<BehaviorComponent>() : entity.assign<BehaviorComponent>();
+	component->behaviors.push_back( behavior );
+	return behavior;
+}
+
+inline std::shared_ptr<BehaviorLambda> assignBehavior( entityx::Entity entity, const BehaviorLambda::Lambda &lambda )
+{
+	auto behavior = std::make_shared<BehaviorLambda>( entity, lambda );
 	auto component = entity.has_component<BehaviorComponent>() ? entity.component<BehaviorComponent>() : entity.assign<BehaviorComponent>();
 	component->behaviors.push_back( behavior );
 	return behavior;
