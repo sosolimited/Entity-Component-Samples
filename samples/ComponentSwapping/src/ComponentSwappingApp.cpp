20968ce07995c5d374cba9585a5168e75dd8baa6
David Wicks <david@sosolimited.com>
2015-10-15 17:52:34 -0400

    Changing multiple components to animate properties of entity.

--- samples/ComponentSwapping/src/ComponentSwappingApp.cpp
+++ samples/ComponentSwapping/src/ComponentSwappingApp.cpp
@@ -12,6 +12,103 @@ using namespace ci::app;
 using namespace std;
 using namespace soso;
 
+///
+/// Each struct can be used as a component to indicate properties and behaviors of an entity.
+///
+struct Oscillate
+{
+	ci::vec2	amount = ci::vec2(2.0f, 0.0f);
+	double		time = 0.0;
+};
+
+struct Grow
+{
+	Grow() = default;
+	Grow(float rate)
+	: rate(rate)
+	{}
+
+	float rate = 1.0f;
+};
+
+struct Scintillate
+{
+	float rate = 1.0f;
+};
+
+struct Position
+{
+	ci::vec2 position;
+};
+
+struct Circle
+{
+	float radius = 24.0f;
+};
+
+struct HSVColor
+{
+	ci::vec3 hsv = ci::vec3(1.0f);
+};
+
+///
+/// Free functions are the simplest kind of system.
+/// They each look through the collection of entities
+/// select, one or more components and do one thing based on those components.
+///
+
+void applyOscillation(entityx::EntityManager &entities, double dt)
+{
+	entityx::ComponentHandle<Oscillate> w;
+	entityx::ComponentHandle<Position> p;
+	for (auto __unused e: entities.entities_with_components(p, w))
+	{
+		w->time += dt;
+		p->position += (float)cos(w->time) * w->amount;
+	}
+}
+
+void applyGrowth(entityx::EntityManager &entities, double dt)
+{
+	entityx::ComponentHandle<Grow> g;
+	entityx::ComponentHandle<Circle> c;
+
+	for (auto __unused e: entities.entities_with_components(g, c))
+	{
+		c->radius += g->rate * dt;
+	}
+}
+
+void applyScintillation(entityx::EntityManager &entities, double dt)
+{
+	entityx::ComponentHandle<HSVColor>		color;
+	entityx::ComponentHandle<Scintillate> scint;
+	for (auto __unused e: entities.entities_with_components(scint, color))
+	{
+		color->hsv.x += scint->rate * dt;
+		if (color->hsv.x > 1.0f) {
+			color->hsv.x -= 1.0f;
+		}
+	}
+}
+
+void drawEntities(entityx::EntityManager &entities)
+{
+	entityx::ComponentHandle<Position>  p;
+	entityx::ComponentHandle<Circle>		c;
+
+  for (auto __unused e: entities.entities_with_components(p, c))
+  {
+		gl::ScopedColor color_scope(Color::white());
+
+		auto color = e.component<HSVColor>();
+		if (color) {
+			gl::color(Color(CM_HSV, color->hsv));
+		}
+		gl::drawSolidCircle(p->position, c->radius);
+  }
+}
+
 class ComponentSwappingApp : public App
 {
 public:
@@ -20,13 +117,15 @@ public:
   void update() override;
   void draw() override;
 
-  void createTestEntities();
+	/// On KeyDown, our application adds and removes components from our test entity.
+	void keyDown(KeyEvent event) override;
 
 private:
   entityx::EventManager  _events;
   entityx::EntityManager _entities;
   entityx::SystemManager _systems;
   ci::Timer              _frame_timer;
+	entityx::Entity				 _entity;
 };
 
 ComponentSwappingApp::ComponentSwappingApp()
@@ -34,22 +133,53 @@ ComponentSwappingApp::ComponentSwappingApp()
   _systems(_entities, _events)
 {}
 
+void ComponentSwappingApp::keyDown(cinder::app::KeyEvent event)
+{
+	switch (event.getCode())
+	{
+		case KeyEvent::KEY_1:
+			if (_entity.has_component<Oscillate>()) {
+				_entity.remove<Oscillate>();
+			}
+			else {
+				_entity.assign<Oscillate>();
+			}
+		break;
+		case KeyEvent::KEY_2:
+			if (_entity.has_component<Grow>()) {
+				_entity.remove<Grow>();
+			}
+			else {
+				auto rate = event.isShiftDown() ? -18.0f : 18.0f;
+				_entity.assign<Grow>(rate);
+			}
+		break;
+		case KeyEvent::KEY_3:
+			if (_entity.has_component<Scintillate>()) {
+				_entity.remove<Scintillate>();
+				_entity.remove<HSVColor>();
+			}
+			else {
+				_entity.assign<Scintillate>();
+				_entity.assign<HSVColor>();
+			}
+		break;
+		default:
+		break;
+	}
+}
+
 void ComponentSwappingApp::setup()
 {
   _systems.add<BehaviorSystem>(_entities);
   _systems.add<ExpiresSystem>();
   _systems.configure();
 
-  createTestEntities();
-}
-
-void ComponentSwappingApp::createTestEntities()
-{
-  for (auto i = 0; i < 72; i += 1)
-  {
-    auto e = _entities.create();
-    e.assign<Expires>(randFloat(1.0f, 100.0f));
-  }
+	_entity = _entities.create();
+	_entity.assign<Circle>();
+	auto pos = _entity.assign<Position>();
+	pos->position = getWindowCenter();
+	_entity.assign<Oscillate>();
 }
 
 void ComponentSwappingApp::update()
@@ -63,27 +193,16 @@ void ComponentSwappingApp::update()
 
   _systems.update<BehaviorSystem>(dt);
   _systems.update<ExpiresSystem>(dt);
+	applyOscillation(_entities, dt);
+	applyGrowth(_entities, dt);
+	applyScintillation(_entities, dt);
 }
 
 void ComponentSwappingApp::draw()
 {
   gl::clear();
 
-  auto position = vec2(10);
-  for (auto e : _entities.entities_with_components<Expires>())
-  {
-    auto ec = e.component<Expires>();
-    auto index = to_string(e.id().index());
-    auto time_remaining = to_string(ec->time);
-    gl::drawString(index + " expires in " + time_remaining + " seconds.", position);
-
-    position += vec2(0, 20);
-    if (position.y > getWindowHeight())
-    {
-      position.y = 10;
-      position.x += 210;
-    }
-  }
+	drawEntities(_entities);
 }
 
 CINDER_APP( ComponentSwappingApp, RendererGl )
