52f9307053d0eb3e4ecde54b2a2186568c8cb2e9
hperigo <hperigo@gmail.com>
2018-01-17 16:15:07 -0200

    added inline keyword

--- src/soso/HierarchyComponent.h
+++ src/soso/HierarchyComponent.h
@@ -41,41 +41,41 @@ public:
   }
 
   /// Append an entity to this hierarchy. Returns a handle to that entity's HierarchyComponentT.
-  Handle  appendChild(entityx::Entity entity);
-  void    appendChild(Handle handle);
+  inline Handle  appendChild(entityx::Entity entity);
+  inline void    appendChild(Handle handle);
   /// Remove this branch from its parent hierarchy. Safe to call whether parent exists or not.
-  void    removeFromParent();
+  inline void    removeFromParent();
 
   /// Returns the entity this component is attached to.
   /// Used during iteration
-  entityx::Entity entity() const { return _entity; }
+  inline entityx::Entity entity() const { return _entity; }
   /// Destroys this component's underlying entity (and hence this component).
-  void destroy() { entity().destroy(); }
+  inline void destroy() { entity().destroy(); }
 
   /// Returns true iff this is the root of a hierarchy (has no parent).
-  bool isRoot() const { return ! _parent; }
+  inline bool isRoot() const { return ! _parent; }
   /// Returns true iff this is the leaf of a hierarchy (has no children).
-  bool isLeaf() const { return _children.empty(); }
+  inline bool isLeaf() const { return _children.empty(); }
   /// Returns a handle to this branch's parent.
-  Handle parent() const { return _parent; }
+  inline Handle parent() const { return _parent; }
   /// Returns a reference to this branch's _children.
-  const std::vector<Handle>& children() { return _children; }
+  inline const std::vector<Handle>& children() { return _children; }
   /// Returns the number of direct _children in this hierarchy.
-  size_t numChildren() const { return _children.size(); }
-  void destroyChildren();
+  inline size_t numChildren() const { return _children.size(); }
+  inline void destroyChildren();
 
   /// Visit all of this components' descendents depth-first. TODO: separate visitor patterns from data.
-  void descend(const std::function<void (const HierarchyComponent &parent, HierarchyComponent &child)> &fn);
+  inline void descend(const std::function<void (const HierarchyComponent &parent, HierarchyComponent &child)> &fn);
   /// Visit all of this components' ancestors. TODO: separate visitor patterns from data.
-  void ascend(const std::function<void (const HierarchyComponent &child, HierarchyComponent &parent)> &fn);
+  inline void ascend(const std::function<void (const HierarchyComponent &child, HierarchyComponent &parent)> &fn);
 
   /// Visit all of this components' descendents depth-first until the visitor function returns false.
   /// Stops descending when function returns true.
-  void descendUntil(const std::function<bool (const HierarchyComponent &parent, HierarchyComponent &child)> &fn);
+  inline void descendUntil(const std::function<bool (const HierarchyComponent &parent, HierarchyComponent &child)> &fn);
 
   /// Visit all of this components' ancestors until the visitor function returns false.
   /// Stops ascending when function returns true.
-  void ascendUntil(const std::function<bool (const HierarchyComponent &child, HierarchyComponent &parent)> &fn);
+  inline void ascendUntil(const std::function<bool (const HierarchyComponent &child, HierarchyComponent &parent)> &fn);
 
 private:
   entityx::Entity            _entity;
@@ -83,13 +83,13 @@ private:
   std::vector<Handle>        _children;
 
   /// Returns a handle to this component.
-  Handle handle() { return _entity.component<HierarchyComponent>(); }
+  inline Handle handle() { return _entity.component<HierarchyComponent>(); }
 
   /// Remove an entity from this hierarchy. Use handle->removeFromParent() to safely remove an item from its hierarchy.
-  void removeChild(Handle handle);
+  inline void removeChild(Handle handle);
 };
 
-#pragma mark - Template Implementation
+//#pragma mark - Template Implementation
 
 void HierarchyComponent::removeFromParent()
 {
--- src/soso/HierarchyComponentT.h
+++ src/soso/HierarchyComponentT.h
@@ -43,42 +43,42 @@ public:
   }
 
   /// Append an entity to this hierarchy. Returns a handle to that entity's HierarchyComponentT.
-  Handle  appendChild(entityx::Entity entity);
-  void    appendChild(Handle handle);
+  inline Handle  appendChild(entityx::Entity entity);
+  inline void  appendChild(Handle handle);
   /// Remove this branch from its parent hierarchy. Safe to call whether parent exists or not.
-  void    removeFromParent();
+  inline void    removeFromParent();
 
   /// Returns the entity this component is attached to.
   /// Used during iteration
-  entityx::Entity entity() const { return _entity; }
+  inline entityx::Entity entity() const { return _entity; }
   /// Destroys this component's underlying entity (and hence this component).
-  void destroy() { entity().destroy(); }
+  inline void destroy() { entity().destroy(); }
 
   /// Returns true iff this is the root of a hierarchy (has no parent).
-  bool isRoot() const { return ! _parent; }
+  inline bool isRoot() const { return ! _parent; }
   /// Returns true iff this is the leaf of a hierarchy (has no children).
-  bool isLeaf() const { return _children.empty(); }
+  inline bool isLeaf() const { return _children.empty(); }
   /// Returns a handle to this branch's parent.
-  Handle parent() const { return _parent; }
+  inline Handle parent() const { return _parent; }
   /// Returns a reference to this branch's _children.
-  const std::vector<Handle>& children() const { return _children; }
+  inline const std::vector<Handle>& children() const { return _children; }
   /// Returns the number of direct _children in this hierarchy.
-  size_t numChildren() const { return _children.size(); }
-  size_t numDescendants() const;
-  void destroyChildren();
+  inline size_t numChildren() const { return _children.size(); }
+  inline size_t numDescendants() const;
+  inline void destroyChildren();
 
   /// Visit all of this components' descendents depth-first. TODO: separate visitor patterns from data.
-  void descend(const std::function<void (const Derived &parent, Derived &child)> &fn);
+  inline void descend(const std::function<void (const Derived &parent, Derived &child)> &fn);
   /// Visit all of this components' ancestors. TODO: separate visitor patterns from data.
-  void ascend(const std::function<void (const Derived &child, Derived &parent)> &fn);
+  inline void ascend(const std::function<void (const Derived &child, Derived &parent)> &fn);
 
   /// Visit all of this components' descendents depth-first until the visitor function returns false.
   /// Stops descending when function returns true.
-  void descendUntil(const std::function<bool (const Derived &parent, Derived &child)> &fn);
+  inline void descendUntil(const std::function<bool (const Derived &parent, Derived &child)> &fn);
 
   /// Visit all of this components' ancestors until the visitor function returns false.
   /// Stops ascending when function returns true.
-  void ascendUntil(const std::function<bool (const Derived &child, Derived &parent)> &fn);
+  inline void ascendUntil(const std::function<bool (const Derived &child, Derived &parent)> &fn);
 
 private:
   entityx::Entity            _entity;
@@ -86,16 +86,16 @@ private:
   std::vector<Handle>        _children;
 
   /// Returns a pointer to this as derived type.
-  Derived* self() { return static_cast<Derived*>(this); }
-  const Derived* self() const { return static_cast<const Derived*>(this); }
+  inline Derived* self() { return static_cast<Derived*>(this); }
+  inline const Derived* self() const { return static_cast<const Derived*>(this); }
   /// Returns a handle to this component.
-  Handle handle() { return _entity.component<Derived>(); }
+  inline Handle handle() { return _entity.component<Derived>(); }
 
   /// Remove an entity from this hierarchy. Use handle->removeFromParent() to safely remove an item from its hierarchy.
-  void removeChild(entityx::ComponentHandle<Derived> handle);
+  inline void removeChild(entityx::ComponentHandle<Derived> handle);
 };
 
-#pragma mark - Template Implementation
+//#pragma mark - Template Implementation
 
 template <typename T>
 void HierarchyComponentT<T>::removeFromParent()
